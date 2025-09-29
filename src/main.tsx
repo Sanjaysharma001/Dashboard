cf1f4348b5817539c4b9dc1dc72b1a32a73780c2
sanjay <sanjaysharmamail08@gmail.com>
2025-09-29 23:29:01 +0530

    Sidebar Update

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Dashboard/"> {/* <--- important for GitHub Pages */}
+    <BrowserRouter> {/* <-- NO basename here */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
